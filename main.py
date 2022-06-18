8b86f1dbc5c33c697304ebdae1b365e4e369c61c
hotratx <mgviero@gmail.com>
2022-06-18 15:53:46 -0300

    add recursion to find and extract .xml from folder extract

--- main.py
+++ main.py
@@ -54,20 +54,25 @@ class Unzip:
             backup: Path da pasta de backup que armazena os arquivos originais .zip
         """
         folder = path.parent
+        folder_temp = path.parent / 'temp'
         backup = path.parent / "backup"
-        return [folder, backup]
+        return [folder_temp, folder, backup]
 
-    def _create_folders(self, folder: Path, backup: Path):
+    def _create_folders(self, folder_temp,  folder: Path, backup: Path):
         """Cria as novas pastas
 
         Args:
-            folder: path da pasta referente ao mês
-            folder: path da pasta que irá receber dados extraídos
-            backup: path da pasta de backup
+            folder_temp: Path da pasta referente temporaria onde serão extraidos
+                os arquivos.xml
+            folder: Path da pasta que irá receber dados extraídos
+            backup: Path da pasta de backup
 
         Return:
             None
         """
+        if not folder_temp.exists():
+            folder_temp.mkdir()
+
         if not folder.exists():
             folder.mkdir()
 
@@ -84,18 +89,19 @@ class Unzip:
         Return:
             None
         """
-        folder, backup = self._handle_names(path)
+        folder_temp, folder, backup = self._handle_names(path)
 
-        self._create_folders(folder, backup)
+        self._create_folders(folder_temp, folder, backup)
 
-        self._unzip(path, folder, backup)
+        self._unzip(path, folder_temp, folder, backup)
 
-    def _unzip(self, file_name: Path, folder: Path, backup: Path) -> None:
+    def _unzip(self, file_name: Path, folder_temp, folder: Path, backup: Path) -> None:
         """Extrai os arquivos zipados para a nova pasta, e move o
-        arquivo original .zip para a pasta de backup
+        arquivo.zip original para a pasta de backup
 
         Args:
             file_name: Path do arquivo original
+            folder_temp: Path da pasta temporária
             folder: Path da pasta referente ao mês
 
         Return:
@@ -103,19 +109,53 @@ class Unzip:
         """
         if file_name.suffix == '.zip':
             with ZipFile(str(file_name), "r") as zip:
-                zip.extractall(str(folder))
+                zip.extractall(str(folder_temp))
         elif file_name.suffix == '.rar':
             with rarfile.RarFile(str(file_name)) as rar:
-                rar.extractall(str(folder))
-        try:
-            shutil.move(file_name, backup)
-        except:
-            print(
-                f"o arquivo {file_name} já está na pasta de backup, logo será deletado!"
-            )
-            file_name.unlink()
+                rar.extractall(str(folder_temp))
 
+        self._remove_files_folder(folder_temp, folder)
+        self._move(file_name, backup)
         self.quant_unzip += 1
+
+    def _move(self, path1: Path, path2: Path):
+        """Move o arquivo do path1 para o folder path2
+
+        Args:
+            path1: Path do arquivo que deve ser movido para o Path2
+            path2: Path da pasta onde será movido o arquivo do path1
+
+        Returns:
+            None
+        """
+        try:
+            shutil.move(path1, path2)
+        except OSError as e:
+            print(f"{e}")
+            # file_name.unlink()
+
+    def _remove_files_folder(self, folder_temp: Path, folder: Path):
+        """Move todos os arquivos.xml da pasta folder_temp para a pasta folder
+
+        Args:
+            folder_temp: Path da pasta temporária onde foi extraidos os arquivos 
+            folder: Pathh da pasta onde os arquivos.xml devem ser extraídos.
+
+        Returns:
+            None
+        """
+        folders = [folder_temp]
+        while True:
+            try:
+                path = folders.pop()
+            except IndexError:
+                shutil.rmtree(folder_temp, ignore_errors=False, onerror=None)
+                break
+            for path in path.iterdir():
+                if path.suffix == '.xml':
+                    self._move(path, folder)
+                elif path.is_dir():
+                    folders.append(path)
 
     def _analise_folder(self, folders_empresas) -> None:
         """Percore a lista de Paths das empresas
